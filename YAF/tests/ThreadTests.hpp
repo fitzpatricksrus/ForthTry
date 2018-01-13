90445c87f11593cf9d39d3110bf53fff602b04ff
fitzpatricksrus <junk@fitzpatircksr.us>
2018-01-12 20:30:27 -0800

    Factored tests into their own classes

---
+++ YAF/tests/ThreadTests.hpp
@@ -0,0 +1,17 @@
+//
+//  ThreadTests.hpp
+//  YAF
+//
+//  Created by Dad on 1/12/18.
+//  Copyright © 2018 Dad. All rights reserved.
+//
+
+#ifndef ThreadTests_hpp
+#define ThreadTests_hpp
+
+class ThreadTests {
+public:
+	static void doTest();
+};
+
+#endif /* ThreadTests_hpp */
