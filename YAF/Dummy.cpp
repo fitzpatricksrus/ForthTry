a4bcbdd64f33abb3883fbdfd23b03fb22dee7cd2
fitzpatricksrus <junk@fitzpatircksr.us>
2018-01-25 18:38:11 -0800

    Dummy commit

---
+++ YAF/Dummy.cpp
@@ -0,0 +1,9 @@
+//
+//  Dummy.cpp
+//  YAF
+//
+//  Created by Dad on 1/25/18.
+//  Copyright © 2018 Dad. All rights reserved.
+//
+
+#include "Dummy.hpp"
---
+++ YAF/Dummy.hpp
@@ -0,0 +1,14 @@
+//
+//  Dummy.hpp
+//  YAF
+//
+//  Created by Dad on 1/25/18.
+//  Copyright © 2018 Dad. All rights reserved.
+//
+
+#ifndef Dummy_hpp
+#define Dummy_hpp
+
+#include <stdio.h>
+
+#endif /* Dummy_hpp */
